857aaf4370362c0f5e488f0b436fe003d79d95b9
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-10-07 23:47:53 +0600

    Full project update: ERP_PROJECT – new apps, templates, settings

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -89,8 +89,15 @@ TEMPLATES = [
 # 🗄️ Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
