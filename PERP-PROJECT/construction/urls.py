8cd559259581676d069bda7f257c81818d8f48bb
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-10-10 13:31:18 +0600

    Full project update: ERP_PROJECT – new apps, templates, settings

--- PERP-PROJECT/construction/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'construction'
-urlpatterns = [
-    path('project-detailed/', views.construction_pd_list, name='construction_pd_list'),
-    
-]
--- PERP-PROJECT/design/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'design'
-
-urlpatterns = [
-    path('client-detailed/', views.design_cd_list, name='design_cd_list'),
-]
-
